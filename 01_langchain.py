124ad1f7f02be802a7b3a8fd623964c384b89738
Choi-Jung-Hyeon <fourmi103@g.skku.edu>
2026-02-11 10:40:46 +0900

    새 파일 추가: Langchain을 사용한 GPT-4.1 모델 호출 기능 구현

---
+++ 01_langchain.py
@@ -0,0 +1,19 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv("key.env")
+
+llm=ChatOpenAI(model_name="gpt-4.1-mini", temperature=0)
+prompt=ChatPromptTemplate.from_template([
+    ("system", "당신은 친절한 선생님입니다. 초등학생도 이해할 수 있게 설명해주세요."),
+    ("user", "{question}"),
+])
+
+#prompt와 llm을 파이프로 연결
+chrain = prompt | llm
+
+#llm 단독호출
+response = llm.invoke("what is langchain?")
+print(response.content)
--- langchain.py
+++
@@ -1,7 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-llm=ChatOpenAI(model_name="gpt-4.1-mini", temperature=0)
-
-#llm 단독호출
-response = llm.invoke("what is langchain?")
-print(response.content)
